039c117d1fbaf00ec5259cd6863e2f44655ee1ae
Matuxa1503 <muravskiy.03@mail.ru>
2025-05-05 13:30:50 +0300

    add plate component

---
+++ frontend/src/pages/Tasks/TasksList/Plate/Plate.tsx
@@ -0,0 +1,20 @@
+// for add svg
+/// <reference types="vite-plugin-svgr/client" />
+import { FC } from 'react';
+import UnfulfilledTick from '@/assets/icons/unfulfilledTick.svg?react';
+import s from './Plate.module.scss';
+
+export const Plate: FC = () => {
+  return (
+    <div className={s.plate}>
+      <div className={s.block}>
+        <UnfulfilledTick width={30} height={30} />
+        <div className={s.taskTitle}>Сходить в душ</div>
+      </div>
+      <div className={s.block}>
+        <div className={s.date}>3 Feb</div> {/* первые 3 буквы показывать */}
+        <div className={s.time}>12:00</div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Tasks/TasksList/TasksList.tsx
+++ frontend/src/pages/Tasks/TasksList/TasksList.tsx
@@ -1,22 +1,15 @@
-// for add svg
-/// <reference types="vite-plugin-svgr/client" />
 import { FC } from 'react';
-import UnfulfilledTick from '@/assets/icons/unfulfilledTick.svg?react';
+import { Plate } from './Plate/Plate';
 import s from './TasksList.module.scss';
 
 export const TasksList: FC = () => {
   return (
     <div className={s.blockTasks}>
       <h2 className={s.subtitle}>This month</h2>
-      <div className={s.plate}>
-        <div className={s.block}>
-          <UnfulfilledTick width={30} height={30} />
-          <div className={s.taskTitle}>Сходить в душ</div>
-        </div>
-        <div className={s.block}>
-          <div className={s.date}>3 Feb</div> {/* первые 3 буквы показывать */}
-          <div className={s.time}>12:00</div>
-        </div>
+      <div className={s.blockPlates}>
+        <Plate />
+        <Plate />
+        <Plate />
       </div>
     </div>
   );
