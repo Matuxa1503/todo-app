7859a76ea70d53be2af5a65e3a321f328e821934
Matuxa1503 <muravskiy.03@mail.ru>
2025-07-12 13:17:50 +0300

    refactor: Updated root route in App.tsx

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,9 +1,5 @@
-import { Navigate } from 'react-router-dom';
 import { ROUTES_NAMES } from '../constants/routes';
 import { IRoutes } from '../interfaces/IRoutes';
 import { TasksContainer } from '../components/TasksContainer/Tasks';
 
-export const ROUTES: IRoutes[] = [
-  { path: ROUTES_NAMES.ROOT, component: () => <Navigate to={'/tasks'} replace /> },
-  { path: ROUTES_NAMES.TASKS, component: TasksContainer },
-];
+export const ROUTES: IRoutes[] = [{ path: ROUTES_NAMES.ROOT, component: TasksContainer }];
